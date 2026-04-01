8f5432612a1c08a7ea61200522a7bcd668762345
Jun Aishima <jaishima@bnl.gov>
2026-04-01 07:44:20 -0400

    Revert "remove slack code"
    This reverts commit 5e83da0636eb266d370ab557a7e7966aac31e83d.

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -25,6 +25,56 @@ def get_api_key_from_env():
     return api_key
 
 
+def slack(func):
+    """
+    Send a message to mon-prefect slack channel about the flow-run status.
+    Send a message to mon-bluesky slack channel if the bluesky-run failed.
+
+    NOTE: the name of this inner function is the same as the real end_of_workflow() function because
+    when the decorator is used, Prefect sees the name of this inner function as the name of
+    the flow. To keep the naming of workflows consistent, the name of this inner function had to match the expected name.
+    """
+
+    def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
+        flow_run_name = FlowRunContext.get().flow_run.dict().get("name")
+
+        # Load slack credentials that are saved in Prefect.
+        mon_prefect = SlackWebhook.load("mon-prefect")
+        mon_bluesky = SlackWebhook.load("mon-bluesky")
+
+        # Get the uid.
+        uid = stop_doc["run_start"]
+
+        # Get the scan_id.
+        run = get_run(uid, api_key=api_key)
+        scan_id = run.start["scan_id"]
+
+        # Send a message to mon-bluesky if bluesky-run failed.
+        if stop_doc.get("exit_status") == "fail":
+            mon_bluesky.notify(
+                f":bangbang: {CATALOG_NAME} bluesky-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```reason: {stop_doc.get('reason', 'none')}```"
+            )
+
+        try:
+            result = func(stop_doc, api_key=api_key, dry_run=dry_run)
+
+            # Send a message to mon-prefect if flow-run is successful.
+            mon_prefect.notify(
+                f":white_check_mark: {CATALOG_NAME} flow-run successful. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}```"
+            )
+            return result
+        except Exception as e:
+            tb = traceback.format_exception_only(e)
+
+            # Send a message to mon-prefect if flow-run failed.
+            mon_prefect.notify(
+                f":bangbang: {CATALOG_NAME} flow-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            )
+            raise
+
+    return end_of_run_workflow
+
+
 @task
 def log_completion():
     logger = get_run_logger()
@@ -32,6 +82,7 @@ def log_completion():
 
 
 @flow
+@slack
 def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
     logger = get_run_logger()
     uid = stop_doc["run_start"]
